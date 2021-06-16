1c0fe357844ba4017b21dd502716a58f9a23d9b8
Felipe Larraguibel <felipelarra.araya@gmail.com>
2021-06-16 16:50:57 -0300

    Implementacion de endPoints de login con sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ServicesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,7 +20,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Servicios
 Route::get('servicios', [ ServicesController::class,'index']);
 Route::post('servicios', [ ServicesController::class,'store']);
 Route::get('servicios/{servicio}', [ ServicesController::class,'show']);
 Route::post('servicios/{id}', [ ServicesController::class,'update']);
+
+
+//Auth Login
+Route::post('register', [ AuthController::class,'register']);
+Route::post('login', [ AuthController::class,'login']);
+//Route::post('userInfo', [ AuthController::class,'userInfo'])->middleware('auth:sanctum');
+//Route::post('logout', [ AuthController::class,'logout'])->middleware('auth:sanctum');
+
+Route::group(['middleware' => ['auth:sanctum']] , function () {
+    Route::post('userInfo', [ AuthController::class,'userInfo']);
+    Route::post('logout', [ AuthController::class,'logout']);
+});
